863f6338ff457f20d91e4d4944e362836e96330c
Rodolfo Morales <rodolfomlphx@gmail.com>
2022-04-12 22:01:17 -0700

    added review component

--- frontend/src/Components/Review.jsx
+++ frontend/src/Components/Review.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
+
 function Review({review}) {
   return (
-    <div className="review">
-        fs
+    <div className="col-1-of-3 review">
+        <span className="title">
+            {review.phrase}
+        </span><br></br>
+        <span className="margin-top-medium review_text">
+            {review.rev}
+        </span>
+
+        <div className="review__profile">
+          <div className="review__profile-left">
+            <img src={review.image} className="review__profile-picture" />
+          </div>
+
+          <div className="review__profile-right">
+            <span className="title">
+              {review.name}
+          </span>
+          <p className=" header__action-supplementarytext">
+            {review.place}
+          </p>
+          </div>
+
+        </div>
     </div>
   )
 }
--- frontend/src/Components/Reviews.jsx
+++ frontend/src/Components/Reviews.jsx
@@ -4,13 +4,16 @@ import Review from './Review'
 function Reviews() {
   return (
     <div className="reviews">
-        <h1 className="header__action-title margin-bottom-medium">
+      <div className="margin-bottom-large">
+      <h1 className="header__action-title margin-bottom-medium">
             Here's What Customers Are Saying
           </h1>
           <span className="card__title-type">Rated 4.6 Out Of 5 Stars By Our Customers</span>
 
+      </div>
+        
 
-          <div className="row">
+          <div className="row ">
           {reviews.map(review => (
                 <Review key={review._id} className="col-1-of-3" review={review} />
             ))}
