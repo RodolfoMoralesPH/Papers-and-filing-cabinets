4aa7d91bf16fec983fb1010d54842a7fc6130158
Rodolfo Morales <rodolfomlphx@gmail.com>
2022-04-17 08:29:12 -0700

    moved photos from public folder to src

---
+++ frontend/src/Components/HomeHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HomeHeader() {
+  return (
+    <div className="homeheader">
+        <div className="homeheader__backgroundimg"></div>
+        <div className="homeheader__profile--img"></div>
+        <div className="home-header__profile--info"></div>
+    </div>
+  )
+}
+
+export default HomeHeader
---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeHeader from '../Components/HomeHeader'
+
+function Home() {
+  return (
+    <main>
+        <HomeHeader />
+    </main>
+  )
+}
+
+export default Home
