e8f70de248cc71ae552af36db1c9e87525254785
Rodolfo Morales <rodolfomlphx@gmail.com>
2022-04-21 22:56:40 -0700

    migrated my data base and fixed an error in rest_framework installatin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'djangorestframework',
+    'rest_framework',
     'base',
     
 ]
--- backend/base/models.py
+++ backend/base/models.py
@@ -2,19 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Visit(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+    def __str__(self):
+        return self.name
+
+
 class Client(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
     profileImage = models.ImageField(null=True, blank=True)
     backgroundImage = models.ImageField(null=True, blank=True)
     visitFrequency = models.CharField(max_length=200, null=True, blank=True)
-    visits = models.ForeignKey(Visit, many=True, null=True, blank=True)
+    visits = models.ForeignKey(Visit, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.name
 class User(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
     profileImage = models.ImageField(null=True, blank=True)
-    clients = models.ForeignKey(Client, many=True, null=True, blank=True)
+    clients = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True, blank=True)
 
-    
-class Visit(models.Model):
+    def __str__(self):
+        return self.name
+
+
+
